1e07b409534514b1d213979125a6bf9d08818d2d
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2023-08-07 19:12:56 +0100

    fix flex responsive issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import { Header, SubHeader, ChartComponent, PieSection, SideBar } from './compon
 
 export default function Home() {
   return ( 
-    <div className="flex grid-cols-12">
-      <div className="col-span-1 bg-[#1e252d]">
+    <div className="flex ">
+      <div className="bg-[#1e252d] ">
         <SideBar />
       </div>
-      <main className="px-10 py-4 col-span-11">
+      <main className="px-10 py-4 min-w-0">
         <Header />
         <SubHeader />
         <ChartComponent />
         <PieSection />
-    </main>
+     </main>
     </div>
     
 

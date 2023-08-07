95a6e40bc6f918a93885b71a01804643c447fe9b
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2023-08-07 16:20:40 +0100

    undo fix

--- app/components/Molecules/SelectItem.tsx
+++ app/components/Molecules/SelectItem.tsx
@@ -1,28 +1,25 @@
-import React, { forwardRef, ReactNode } from 'react';
+import React, { forwardRef } from 'react';
 import * as Select from '@radix-ui/react-select';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import { CheckIcon } from '@radix-ui/react-icons';
 
-interface SelectItemProps extends React.ComponentProps<typeof Select.Item> {
-  value: string;
-  children: ReactNode;
-  className?: string;
-}
+type SelectItemProps = React.ComponentProps<typeof Select.Item>;
 
-const SelectItem = forwardRef<HTMLLIElement, SelectItemProps>(
-  ({ value, children, className, ...props }, forwardedRef) => {
+const SelectItem = React.forwardRef<HTMLLIElement, SelectItemProps>(
+  ({ children, ...props }, forwardedRef) => {
     return (
-      <Select.Item
-        className=
-          'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1'
-        {...props}
-        // @ts-ignore
-        ref={forwardedRef}
-      >
-        <Select.ItemText>{children}</Select.ItemText>
-        <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-          <CheckIcon />
-        </Select.ItemIndicator>
-      </Select.Item>
+      <>
+        <Select.Item
+          className="text-[13px] leading-none rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[state=checked]:text-[#174cff] data-[highlighted]:outline-none data-[highlighted]:bg-[#f8fafe] data-[highlighted]:text-[#174cff] cursor-pointer"
+          {...props}
+          // @ts-ignore
+          ref={forwardedRef}
+        >
+          <Select.ItemText>{children}</Select.ItemText>
+          <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+            <CheckIcon />
+          </Select.ItemIndicator>
+        </Select.Item>
+      </>
     );
   }
 );

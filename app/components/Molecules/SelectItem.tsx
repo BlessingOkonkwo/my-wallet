dbd074e66e7d3be6c9e60767a1cbbb269d4cb71f
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2023-08-07 16:06:08 +0100

    fix type error

--- app/components/Molecules/SelectItem.tsx
+++ app/components/Molecules/SelectItem.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import * as Select from '@radix-ui/react-select';
 import { CheckIcon } from '@radix-ui/react-icons';
 
-// @ts-ignore
-const SelectItem = React.forwardRef(({ children, ...props }, forwardedRef) => {
-    return (
-    <>
-      <Select.Item
-        className="text-[13px] leading-none rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[state=checked]:text-[#174cff] data-[highlighted]:outline-none data-[highlighted]:bg-[#f8fafe] data-[highlighted]:text-[#174cff] cursor-pointer"
-        {...props}
-        // @ts-ignore
-        ref={forwardedRef}
-      >
-        <Select.ItemText>{children}</Select.ItemText>
-        <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-          <CheckIcon />
-        </Select.ItemIndicator>
-      </Select.Item>
-        </>
-    );
-});
+type SelectItemProps = React.ComponentProps<typeof Select.Item>;
 
-SelectItem.displayName = "SelectItem"
+const SelectItem = React.forwardRef<HTMLLIElement, SelectItemProps>(
+  ({ children, ...props }, forwardedRef) => {
+    return (
+      <>
+        <Select.Item
+          className="text-[13px] leading-none rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[state=checked]:text-[#174cff] data-[highlighted]:outline-none data-[highlighted]:bg-[#f8fafe] data-[highlighted]:text-[#174cff] cursor-pointer"
+          {...props}
+          // @ts-ignore
+          ref={forwardedRef}
+        >
+          <Select.ItemText>{children}</Select.ItemText>
+          <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+            <CheckIcon />
+          </Select.ItemIndicator>
+        </Select.Item>
+      </>
+    );
+  }
+);
+
+SelectItem.displayName = 'SelectItem';
 export default SelectItem;
